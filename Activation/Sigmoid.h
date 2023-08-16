37f76d22f6e131582afaba623a68a2eb1922fb14
TladiL <62520680+TladiL@users.noreply.github.com>
2023-08-16 20:03:55 +0200

    Sigmoid

--- Activation/Sigmoid.h
+++ Activation/Sigmoid.h
@@ -5,19 +5,18 @@
 
 namespace MiniDNN
 {
-	class Identity
+	class Sigmoid
 	{
 	private:
 		typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
 
 	public:
-		static inline void activation(const Matrix& Z, Matrix& A) { A.noalias() = Z; }
+		static inline void activation(const Matrix& Z, Matrix& A)
+		{ A.array() = Scalar(1) / (Scalar(1) + (-Z.array()).exp()); }
 
 		static inline void apply_jacobian(const Matrix& Z, const Matrix& A, const Matrix& F, Matrix& G)
-		{
-			G.noalias() = F;
-		}
+		{ G.array() = A.array() * (Scalar(1) - A.array()) * F.array(); }
 
-		static std::string return_type() { return "Identity"; }
+		static std::string return_type() { return "Sigmoid"; }
 	};
 }
