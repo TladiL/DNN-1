a77982300cfaff27f3a313691d07c4814beebdec
TladiL <62520680+TladiL@users.noreply.github.com>
2023-08-16 20:58:28 +0200

    commit

--- Activation/Softmax.h
+++ Activation/Softmax.h
@@ -9,13 +9,20 @@ namespace MiniDNN
 	{
 	private:
 		typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
+		typedef Eigen::Array<Scalar, 1, Eigen::Dynamic> RowArray;
 
 	public:
-		static inline void activation(const Matrix& Z, Matrix& A) { A.noalias() = Z; }
+		static inline void activation(const Matrix& Z, Matrix& A)
+		{
+			A.array() = (Z.rowwise() - Z.colwise().maxCoeff()).array().exp();
+			RowArray colsums = A.colwise().sum();
+			A.array().rowwise() /= colsums;
+		}
 
 		static inline void apply_jacobian(const Matrix& Z, const Matrix& A, const Matrix& F, Matrix& G)
 		{
-			G.noalias() = F;
+			RowArray a_dot_f = A.cwiseProduct(F).colwise().sum();
+			G.array() = A.array() * (F.array().rowwise() - a_dot_f);
 		}
 
 		static std::string return_type() { return "Softmax"; }
